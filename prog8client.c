3c82b01ed2a155c8da5a4d622972b37dd8751a38
Keshava Karthik <kkhassan2004@gmail.com>
2025-04-20 23:23:15 +0530

    change

--- prog8client.c
+++ prog8client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #define SERVER_IP "127.0.0.1"
@@ -12,37 +13,52 @@ int main() {
     char buffer[BUFFER_SIZE];
     struct sockaddr_in server_addr;
     FILE *fp;
+    socklen_t addr_size;
+    int bytes_read, total_sent = 0;
 
+    // Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET; 
-    server_addr.sin_port = htons(PORT); 
+    // Server details
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    addr_size = sizeof(server_addr);
 
+    // Input filename
     char filename[100];
     printf("Enter filename to send: ");
     scanf("%s", filename);
 
+    // Open file
     fp = fopen(filename, "r");
-    if (!fp) {
-        perror("File open failed");
+    if (fp == NULL) {
+        perror("Error opening file");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
 
-    int n;
-    while ((n = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
-        sendto(sockfd, buffer, n, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    // Send file content
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
+        if (sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr*)&server_addr, addr_size) < 0) {
+            perror("Error sending file data");
+            fclose(fp);
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        total_sent += bytes_read;
     }
 
-    // Send END to indicate file transmission complete
-    sendto(sockfd, "END", strlen("END"), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    // Send END message to tell server we're done
+    strcpy(buffer, "END");
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, addr_size);
 
-    printf("File sent successfully.\n");
+    printf("File sent successfully (%d bytes).\n", total_sent);
 
     fclose(fp);
     close(sockfd);
--- prog8server.c
+++ prog8server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 #define PORT 8080
@@ -13,16 +14,20 @@ int main() {
     socklen_t addr_len = sizeof(client_addr);
     FILE *fp;
 
+    // Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Server settings
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET; 
     server_addr.sin_addr.s_addr = INADDR_ANY; 
     server_addr.sin_port = htons(PORT); 
 
+    // Bind socket to port
     if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         close(sockfd);
@@ -31,6 +36,7 @@ int main() {
 
     printf("Server is ready. Waiting for file...\n");
 
+    // Open file to write received data
     fp = fopen("received_file.txt", "w");
     if (!fp) {
         perror("File open failed");
@@ -38,13 +44,23 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    int n;
+    int bytes_received;
     while (1) {
-        n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
-        if (n <= 0 || strcmp(buffer, "END") == 0) {
+        memset(buffer, 0, BUFFER_SIZE);  // Clear buffer
+        bytes_received = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
+
+        if (bytes_received < 0) {
+            perror("Error receiving data");
             break;
         }
-        fwrite(buffer, 1, n, fp);
+
+        // Check for end signal
+        if (strncmp(buffer, "END", 3) == 0) {
+            break;
+        }
+
+        // Write data to file
+        fwrite(buffer, 1, bytes_received, fp);
     }
 
     printf("File received and saved as 'received_file.txt'.\n");
