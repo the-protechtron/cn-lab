337ffd00eeaf0a9a83b16f9bf659e5c7777e1181
Keshava Karthik <kkhassan2004@gmail.com>
2025-04-20 23:11:05 +0530

    yes

--- prog7client.c
+++ prog7client.c
@@ -1,22 +1,55 @@
-#include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
-int main(){
-	char buffer[100];
-	int fd,fd1,fd2,n;
-	fd1 = open("fifo1",O_WRONLY);
-	fd2 = open("fifo2",O_RDONLY);	
-	printf("Both fifos openend\n");
-	read(fd2,buffer,sizeof(buffer));
-	printf("Server says %s\n",buffer);
-	write(fd1,"text.txt",9);
-	printf("Requested for a file\n");
-	n=read(fd2,buffer,sizeof(buffer));
-	printf("n value is : %d\n",n);
-	buffer[n]='\0';
-	printf("File content: %s\n",buffer);
-	fd=open("recvd.txt",O_CREAT|O_RDWR);
-	write(fd,buffer,n);
-	close(fd1);
-	close(fd2);
+// mkfifo fifo1
+// mkfifo fifo2
+// do them beforehand
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    char buffer[1024];
+    int fd1, fd2, file_fd, n;
+
+    // Open FIFOs
+    fd1 = open("fifo1", O_WRONLY);  // write to server
+    fd2 = open("fifo2", O_RDONLY);  // read from server
+
+    if (fd1 < 0 || fd2 < 0) {
+        perror("FIFO open failed");
+        return 1;
+    }
+
+    printf("Connected to server using FIFOs\n");
+
+    // Read welcome message
+    read(fd2, buffer, sizeof(buffer));
+    printf("Server says: %s\n", buffer);
+
+    // Send file name to server
+    char filename[] = "text.txt";
+    write(fd1, filename, strlen(filename) + 1);
+    printf("Requested file: %s\n", filename);
+
+    // Read file content from server
+    n = read(fd2, buffer, sizeof(buffer));
+    if (n <= 0) {
+        printf("File not received or empty.\n");
+        return 1;
+    }
+
+    buffer[n] = '\0';  // Null-terminate
+    printf("Received file content:\n%s\n", buffer);
+
+    // Save to file
+    file_fd = open("recvd.txt", O_CREAT | O_WRONLY, 0644);
+    write(file_fd, buffer, n);
+    close(file_fd);
+
+    // Close FIFOs
+    close(fd1);
+    close(fd2);
+
+    return 0;
 }
--- prog7server.c
+++ prog7server.c
@@ -1,29 +1,56 @@
-#include<stdio.h>
-#include<unistd.h>
+// make sure text.txt exists in the same folder
 
-#include<fcntl.h>
-int main(){
-	char buffer[100];
-	int fd,fd1,fd2,n;
-	mkfifo("fifo1",0600);
-	mkfifo("fifo2",0600);
-	fd1 = open("fifo1",O_RDONLY);
-	fd2 = open("fifo2",O_WRONLY);	
-	printf("Both fifos openend\n");
-	write(fd2,"hello",6);
-	read(fd1,buffer,sizeof(buffer));
-	printf("Requested file %s\n",buffer);
-	fd=open(buffer,O_RDONLY);
-	if(fd<0){
-		printf("No Such file exists\n");
-		exit(0);
-	}
-	
-	n=read(fd,buffer,sizeof(buffer));
-	printf("Value of n is %d\n",n);
-	printf("File content: %s\n",buffer);
-	write(fd2,buffer,n);
-	close(fd);
-	close(fd1);
-	close(fd2);
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main() {
+    char buffer[1024];
+    int fd, fd1, fd2, n;
+
+    // Create FIFOs (named pipes) if they don't exist
+    mkfifo("fifo1", 0666);
+    mkfifo("fifo2", 0666);
+
+    // Open FIFOs
+    fd1 = open("fifo1", O_RDONLY); // Read from client
+    fd2 = open("fifo2", O_WRONLY); // Write to client
+
+    if (fd1 < 0 || fd2 < 0) {
+        perror("Error opening FIFOs");
+        exit(1);
+    }
+
+    printf("Connected to client via FIFOs\n");
+
+    // Send welcome message
+    write(fd2, "Hello from server!", 20);
+
+    // Receive file name
+    read(fd1, buffer, sizeof(buffer));
+    printf("Client requested file: %s\n", buffer);
+
+    // Try to open the requested file
+    fd = open(buffer, O_RDONLY);
+    if (fd < 0) {
+        printf("File not found: %s\n", buffer);
+        write(fd2, "ERROR: File not found", 23);
+    } else {
+        // Read file contents
+        n = read(fd, buffer, sizeof(buffer));
+        buffer[n] = '\0';  // Null terminate to print properly
+        printf("Sending file content to client...\n");
+
+        // Send to client
+        write(fd2, buffer, n);
+        close(fd);
+    }
+
+    close(fd1);
+    close(fd2);
+
+    return 0;
 }
