3f27cb43279c978a84e6e31bed6d440ed4c0222e
Keshava Karthik <kkhassan2004@gmail.com>
2025-04-21 06:44:03 +0530

    changes

--- prog4client.c
+++ prog4client.c
@@ -8,54 +8,75 @@
 
 #define BUFFER_SIZE 2048
 
-void error(const char *msg) {
-    perror(msg);
-    exit(0);
-}
-
 int main(int argc, char *argv[]) {
-    int sockfd, portno, n;
+    int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     char filename[256], buffer[BUFFER_SIZE];
+    int bytes_received;
 
+    // Check for correct usage
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
+    // Extract port number
     portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
 
+    // Create TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Resolve server address
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr, "ERROR: no such host\n");
-        exit(0);
+        fprintf(stderr, "ERROR: No such host\n");
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
 
+    // Setup server address structure
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Input filename to request
     printf("Enter filename to request from server: ");
     scanf("%s", filename);
 
-    write(sockfd, filename, strlen(filename));
+    // Send filename to server
+    if (write(sockfd, filename, strlen(filename)) < 0) {
+        perror("Error sending filename");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Receive file content from server
     memset(buffer, 0, sizeof(buffer));
-    n = read(sockfd, buffer, sizeof(buffer) - 1);
-    if (n < 0)
-        error("ERROR reading from socket");
+    bytes_received = read(sockfd, buffer, sizeof(buffer) - 1);
+    if (bytes_received < 0) {
+        perror("Error reading from socket");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Output received content
     printf("\nReceived content from server:\n");
     printf("%s\n", buffer);
 
+    // Cleanup
     close(sockfd);
     return 0;
 }
--- prog4server.c
+++ prog4server.c
@@ -7,66 +7,79 @@
 
 #define BUFFER_SIZE 2048
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
-
 int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
     char buffer[256], filedata[BUFFER_SIZE];
     struct sockaddr_in serv_addr, cli_addr;
     FILE *fp;
-    int n;
+    int bytes_read;
 
+    // Check for port argument
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
+    // Create TCP socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // Setup server address structure
     memset(&serv_addr, 0, sizeof(serv_addr));
     portno = atoi(argv[1]);
-
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
+    // Bind the socket
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Listen for connections
     listen(sockfd, 5);
     printf("Server waiting for client connection...\n");
 
+    // Accept connection
     clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0)
-        error("ERROR on accept");
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (newsockfd < 0) {
+        perror("Accept failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
     // Receive filename
     memset(buffer, 0, sizeof(buffer));
-    n = read(newsockfd, buffer, sizeof(buffer) - 1);
-    if (n < 0)
-        error("ERROR reading from socket");
+    if (read(newsockfd, buffer, sizeof(buffer) - 1) < 0) {
+        perror("Error reading filename");
+        close(newsockfd);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
     printf("Server received filename: %s\n", buffer);
 
+    // Open and read file
     fp = fopen(buffer, "r");
     if (fp == NULL) {
-        printf("File not found.\n");
+        perror("File not found");
         write(newsockfd, "File not found!\n", 17);
     } else {
         memset(filedata, 0, sizeof(filedata));
-        fread(filedata, 1, sizeof(filedata) - 1, fp);
-        write(newsockfd, filedata, strlen(filedata));
+        bytes_read = fread(filedata, 1, sizeof(filedata) - 1, fp);
+        write(newsockfd, filedata, bytes_read);
         fclose(fp);
         printf("File sent to client.\n");
     }
 
+    // Cleanup
     close(newsockfd);
     close(sockfd);
     return 0;
